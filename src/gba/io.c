3f71c3cdea4df4840e5881b9958e2014dba7035f
Diogo Marques <xmarques999@hotmail.com>
2015-09-28 18:45:30 +0100

    added test file to branch

--- src/gba/io.c
+++ src/gba/io.c
@@ -4,7 +4,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #include "io.h"
-
 #include "gba/rr/rr.h"
 #include "gba/serialize.h"
 #include "gba/sio.h"
